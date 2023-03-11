3b3798b4b9156ede862b08a509fecd1a329ae217
Zamzam <Zamzamhassan44@gmail.com>
2023-03-11 22:59:48 +0300

    consumed api and rendered on the browser

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Countries from './components/Countries/Countries';
 
 function App() {
--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Countries.css'
 import { useEffect } from 'react';
 
 function Countries() {
+
+    const [countries, setCountries] = useState([])
 
 useEffect(()=>{
     fetch('https://restcountries.com/v3.1/all')
     .then((response) => response.json())
     .then((data) => {
        console.log(data);
+       setCountries(data)
     })
 })
 
     return (
-      <div className="app">
-      <h1>countries</h1>
-  
-  <div class="wrapper">
-      <div className="header">Avengers Responsive Cards UI Design</div>
-  
-      <div className="cards_wrap">
-          <div className="card_item">
-              <div classNaae="card_inner">
-                  <img src="doctor_strange.png" />
-                  <div class="role_name">Doctor Strange</div>
-                  <div class="real_name">Benedict Cumberbatch</div>
-                  <div class="film">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt aliqua.</div>
-              </div>
-          </div>
-      </div>
-  </div>  
-      </div>
+        
+        <div className="wrapper">
+            <div className="header">Avengers Responsive Cards UI Design</div>
+            {countries.map((country)=>{
+                return(
+        <div className="cards_wrap">
+                        <div className="card_item">
+                            <div className="card_inner">
+                                <img src={country.coatOfArms.svg} />
+                                <div className="role_name">{country.name.common}</div>
+                                <div className="real_name">Benedict Cumberbatch</div>
+                                <div className="film">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt aliqua.</div>
+                            </div>
+                        </div>
+                    </div>
+
+                )
+            })}
+
+          
+
+        </div>   
     );
   }
   
