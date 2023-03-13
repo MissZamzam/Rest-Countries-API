3afacc184f87df7577acc281d6a70d10fd9ade30
Zamzam <Zamzamhassan44@gmail.com>
2023-03-14 00:47:21 +0300

    search feature worked

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Filter.css'
 
+
 const Filter = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = (event) => {
+    const { value } = event.target;
+    setSearchValue(value.toLowerCase());
+  };
+
+  useEffect(() => {
+    const countryName = document.querySelectorAll('.role_name');
+
+    countryName.forEach((name) => {
+      if (name.innerText.toLowerCase().includes(searchValue)) {
+        name.parentElement.parentElement.style.display = 'block';
+      } else {
+        name.parentElement.parentElement.style.display = 'none';
+      }
+    });
+  }, [searchValue]);
+
   return (
   <section className='filter'>
     <form className='form-control'>
-        <input type="search" name='search' id="search"
+    <input id="search" type="text" value={searchValue} onChange={handleSearch} 
         placeholder='Search for a country' />
     </form>
     <div className='region-filter'>
