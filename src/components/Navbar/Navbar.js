d6aaa5f00d78b0665f1c15446abdb1be96cfb9cb
Zamzam <Zamzamhassan44@gmail.com>
2023-03-22 01:51:22 +0300

    navbar style changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,30 +3,17 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-       <nav>
-        <div class="brand">
-            <h3>MyBrand</h3>
-        </div>
-        <div class="navigation">
-            {/* <ul class="menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Team</a></li>
-                <li><a href="#">Pricing</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul> */}
-            <div class="search">
-                <div class="search-content">
-                    <button class="search-button"><i class="fa fa-search"></i></button>
-                    <input type="text" class="search-input" placeholder="Search here..." />
-                </div>
-            </div>
-        </div>
+    <nav>
+        <ul>
+            <li><a href=''>Home</a></li>
+            <li className="search">
+                <form>
+                    <input type="text" placeholder='search...'/>
+                    <button type='submit'>Search</button>
+                </form>
+            </li>
+        </ul>
     </nav>
-    <div class="banner">
-        <img src="https://i.imgur.com/XNwuthr.jpg" alt="banner_image" />
-    </div>
-    </div>
   )
 }
 
