2e6f81baa96220eee55c2eea2b36116a0b24ec63
Zamzam <Zamzamhassan44@gmail.com>
2023-03-22 02:37:38 +0300

    navbar style changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+
+    const [searchValue, setSearchValue] = useState('');
+
+
+    const handleSearch = (event) => {
+        const { value } = event.target;
+        setSearchValue(value.toLowerCase());
+      };
+      // search
+  useEffect(() => {
+    const countryName = document.querySelectorAll('.role_name');
+
+    countryName.forEach((name) => {
+      if (name.innerText.toLowerCase().includes(searchValue)) {
+        name.parentElement.parentElement.style.display = 'block';
+      } else {
+        name.parentElement.parentElement.style.display = 'none';
+      }
+    });
+  }, [searchValue]);
+
+
   return (
     <nav>
         <ul>
-            <li><a href=''>Home</a></li>
+            <li><a href=''>All Countries</a></li>
             <li className="search">
                 <form>
-                    <input type="text" placeholder='search...'/>
-                    <button type='submit'>Search</button>
+                    <input type="text" placeholder='search country...' value={searchValue} onChange={handleSearch} />
+
+                    {/* <button type='submit'>Search</button> */}
                 </form>
             </li>
         </ul>
