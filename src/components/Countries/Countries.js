148e527f6b3498a42232cdfdc167907c3e85f999
Zamzam <Zamzamhassan44@gmail.com>
2023-03-23 00:51:35 +0300

    fixed navbar

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,74 +1,53 @@
-import React, { useState } from 'react';
-import './Countries.css'
-import { useEffect } from 'react';
-import Filter from '../Filter/Filter';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Countries.css";
+import { useEffect } from "react";
+// import Filter from "../Filter/Filter";
+import { Link } from "react-router-dom";
 
 function Countries() {
-    const [countries, setCountries] = useState([])
+  const [countries, setCountries] = useState([]);
 
-useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then((response) => response.json())
-    .then((data) => {
-       console.log(data);
-       setCountries(data)
-    })
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCountries(data);
+      });
   }, []);
 
-    return (
-        <div className='country-app'>  
-        
-                {/* <Filter />   */}
-        <div className="wrapper">
-        <div className="cards_wrap">
-        {countries.map((country)=>{
-                return(
-                        <div className="card_item">
+  return (
+    <div className="country-app">
+      {/* <Filter />   */}
+      <div className="cards_wrap">
+        {countries.map((country) => {
+          const { capital, continents, region, coatOfArms } = country;
 
-                            <div className="card_inner">
-                                <img src={country.coatOfArms.svg} alt="coat of arms"/>
-                                <div className="role_name">{country.name.common}</div>
-                                <div className="real_name">Continent : {country.continents}</div>
-                                <p className='film'>Capital : {country.capital}</p>
-                                {/* <p className='film'>Timezones : {country.timezones}</p> */}
-                                <p className='films'>Region : {country.region}</p>
+          return (
+            <div className="card_item">
+              <div className="card_inner">
+                <img src={coatOfArms.svg} alt="coat of arms" />
+                <div className="role_name">{country.name.common}</div>
+                <div className="real_name">Continent : {continents}</div>
+                <p className="film">Capital : {capital}</p>
 
-                                {/* <Modal name={product.name} description={product.description} /> */}
-                                <Link to='./country'>
-                                <button>Details</button>
-                                </Link>
-                                 </div>
-                             
-{/* 
-                            <div className="card_back">
-                                <h2>{country.name.official}</h2>
-                                <p>Population : {country.population}</p>
-                                <p>{country.borders}</p>
-                                <p>Car : {country.car.side}</p>
-                                <p>{country.flag} </p>
-                                <p>{country.fifa}</p>
-                            </div>
-                             */}
+                <Link
+                  to={{
+                    pathname: "./country",
+                    state: {
+                      name: country.name.common,
+                      capital: country.capital,
+                    }
+                  }}>
+                  <button>Learn More</button>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
-                               
-                        </div>
-
-
-
-                )
-            })}
-
-           
-
-                        
-</div>
-                    </div>   
-        </div>
-
-        
-    );
-  }
-  
-  export default Countries;
-  
+export default Countries;
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,75 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import './Country.css'
 
-const Country = () => {
+const Country = (props) => {
+  const { name, capital } = props.location.state;
+
   return (
+    
+    <div class="container">
+    <a href="#" class="back-link"><i class="fas fa-arrow-left"></i> Back</a>
+    <div class="image">
+      <img src="https://images.unsplash.com/photo-1679471228928-0741589a57fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="Your Image" />
+    </div>
+    <div class="text">
+      <h1>{name}</h1>
+      <p>Capital: {capital}</p>
+      <p>Your text content goes here</p>
+      <p>Your text content goes here</p>
+      <p>Your text content goes here</p>
+      <p>Your text content goes here</p>
+      <p>Your text content goes here</p>
 
-    <>
-    <section className="country">
-      <Link to="/" className="btn btn-light">
-        <i className="fas fa-arrow-left"></i> Back Home
-      </Link>
-      
-
-          <article>
-            <div className="country-inner">
-              <div className="flag">
-                <img src="https://images.unsplash.com/photo-1678753713297-3cca1ed50c91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className='flag' alt />
-              </div>
-
-              <div className="country-details">
-                <div>
-                  <h2>ssssssssv</h2>
-                  <h5>
-                    Native Name: <span>nativeName</span>
-                  </h5>
-                  <h5>
-                    Population: <span>population.toLocaleString</span>
-                  </h5>
-                  <h5>
-                    Region: <span>llllllllllll</span>
-                  </h5>
-                  <h5>
-                    Sub Region: <span>jooooooooooo</span>
-                  </h5>
-                  <h5>
-                    Capital: <span>iiiiiiiiimk</span>
-                  </h5>
-                </div>
-
-                <div>
-                  <h5>
-                    Top Level Domain: <span>sbihkivhbk</span>
-                  </h5>
-                  <h5>
-                    Currencies: <span>dr6yfvhil</span>
-                  </h5>
-                  <h5>
-                    Languages: <span>sernk</span>
-                  </h5>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <h3>Border Countries: </h3>
-              <div className="borders">
-               
-                    <ul>
-                      <li>cssscc</li>
-                      <li>cssscc</li>
-
-                    </ul>
-                  
-              
-              </div>
-            </div>
-          </article>
-      
-    </section>
-</>
-
+      {/* <a href="#">Learn More</a> */}
+    </div>
+  </div>
+  
   )
 }
 
