8c0d95dc05595bd6e028011fb32abc055bd63612
Zamzam <Zamzamhassan44@gmail.com>
2023-04-12 22:53:57 +0300

    added map to the countries cards

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Countries.css";
-// 
+//
 function Countries() {
   const [countries, setCountries] = useState([]);
 
@@ -15,30 +15,37 @@ function Countries() {
 
   return (
     <div className="country-app">
-    
       <div className="cards_wrap">
         {countries.map((country) => {
-          const { name,capital, continents, maps,population, car,coatOfArms,fifa,timezones, flags } = country;
+          const {
+            name,
+            capital,
+            continents,
+            maps,
+            population,
+            car,
+            coatOfArms,
+            fifa,
+            timezones,
+            flags,
+          } = country;
 
           return (
             <div className="card_item">
               <div className="card_inner">
-              <img src={flags.svg} alt="flags" />
+                <img src={flags.svg} alt="flags" />
 
                 <div className="role_name">{name.common}</div>
                 <div className="real_name">Continent : {continents}</div>
                 <p className="real_capital">Capital : {capital}</p>
-                <p className="real_capital"><a href={maps.googleMaps}>View map</a></p>
+                <p className="real_capital">
+                  <a href={maps.googleMaps}>View map</a>
+                </p>
 
                 <h1 className="film film-details">Timezones : {timezones}</h1>
                 <p className="film1 film-details">population : {population}</p>
                 <p className="film2 film-details">Fifa : {fifa}</p>
                 <p className="film3 film-details">Car : {car.side}</p>
-
-
-
-
-
               </div>
             </div>
           );
